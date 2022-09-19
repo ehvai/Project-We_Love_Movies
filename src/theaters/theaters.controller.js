4c08776633b1f4480fb8ebb00b4646ee2b619c51
Erica <ericaewallbridge@gmail.com>
2022-09-19 16:10:45 -0400

    Made an adjustment to theaters controller

--- src/theaters/theaters.controller.js
+++ src/theaters/theaters.controller.js
@@ -1,19 +1,5 @@
 const service = require("./theaters.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
-const reduceProperties = require("../utils/reduce-properties");
-
-const reduceMovies = reduceProperties("theater_id",{
-    movie_id: ["movies",null,"movie_id"],
-    title: ["movies",null,"title"],
-    runtime_in_minutes: ["movies",null,"runtime_in_minutes"],
-    rating: ["movies",null,"rating"],
-    description: ["movies",null,"description"],
-    image_url: ["movies",null,"image_url"],
-    created_at: ["movies",null,"created_at"],
-    updated_at: ["movies",null,"updated_at"],
-    is_showing: ["movies_theaters",null,"is_showing"],
-    theater_id: ["theaters",null,"theater_id"],
-})
 
 async function list(req, res, next){
     res.json({ data: await service.list()});
